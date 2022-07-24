57427513d468e4d50932ee10d54d20750bd33bf2
Mitch Patterson <mpatterson3@hotmail.com>
2022-07-24 23:17:43 +0000

    implemeted templates

--- main.go
+++ main.go
@@ -1,45 +1,52 @@
 package main
 
 import (
-"fmt"
-"log"
-"net/http"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
 
-func order(w http.ResponseWriter, r *http.Request){
-	fmt.Println(r.URL.RawQuery)
-	fmt.Fprintf(w,`This is the order page`)
+var tpl = template.New("foo")
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
-func about(w http.ResponseWriter, r *http.Request){
+
+func order(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.RawQuery)
-	fmt.Fprintf(w,`This is the about page
+	fmt.Fprintf(w, `This is the order page`)
+}
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprintf(w, `This is the about page
 					tell customers who we are`)
 }
-func contact(w http.ResponseWriter, r *http.Request){
+func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.RawQuery)
-	fmt.Fprintf(w,`This is the contact page
+	fmt.Fprintf(w, `This is the contact page
 	tell customers how to contact us`)
 }
-func subscribe(w http.ResponseWriter, r *http.Request){
+func subscribe(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.RawQuery)
-	fmt.Fprintf(w,`This is the subcribe page. get info from subscriber add to database`)
+	fmt.Fprintf(w, `This is the subcribe page. get info from subscriber add to database`)
 }
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.RawQuery)
-	fmt.Fprintf(w, `
-          ##         .
-    ## ## ##        ==
- ## ## ## ## ##    ===
-/"""""""""""""""""\___/ ===
-{                       /  ===-
-\______ O           __/
- \    \         __/
-  \____\_______/
+	// 	fmt.Println(r.URL.RawQuery)
+	// 	fmt.Fprintf(w, `
+	//           ##         .
+	//     ## ## ##        ==
+	//  ## ## ## ## ##    ===
+	// /"""""""""""""""""\___/ ===
+	// {                       /  ===-
+	// \______ O           __/
+	//  \    \         __/
+	//   \____\_______/
 
-	
-Hello from Docker!
+	// Hello from Docker!
 
-`)
+	//`)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
 
 func main() {
@@ -49,7 +56,5 @@ func main() {
 	http.HandleFunc("/contact", contact)
 	http.HandleFunc("/subscribe", subscribe)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	
+
 }
-
-
