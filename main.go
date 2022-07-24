88b1dbfa743428c00d5a1106b73ec95711c4df17
Mitch Patterson <mpatterson3@hotmail.com>
2022-07-24 21:35:56 +0000

    added routes

--- main.go
+++ main.go
@@ -6,6 +6,24 @@ import (
 "net/http"
 )
 
+func order(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprintf(w,`This is the order page`)
+}
+func about(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprintf(w,`This is the about page
+					tell customers who we are`)
+}
+func contact(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprintf(w,`This is the contact page
+	tell customers how to contact us`)
+}
+func subscribe(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprintf(w,`This is the subcribe page. get info from subscriber add to database`)
+}
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.RawQuery)
 	fmt.Fprintf(w, `
@@ -26,6 +44,12 @@ Hello from Docker!
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/order", order)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/subscribe", subscribe)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	
 }
+
 
